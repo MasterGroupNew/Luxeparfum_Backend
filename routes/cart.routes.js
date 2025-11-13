8e60039581994811e79d45a0beac16f9d5f680dd
Abou <jeanrichard1211@gmail.com>
2025-11-13 21:57:53 +0000

    mise à jour du panier

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const cartCtrl = require('../controllers/cartController');
 const auth = require('../middleware/auth.middleware');
 
-router.post('/add', auth, cartCtrl.addToCart);
-router.get('/get', auth, cartCtrl.getCart);
-router.post('/sync', auth, cartCtrl.syncCartFromLocalStorage);
-router.delete('/remove/:productId', auth, cartCtrl.removeFromCart);
+router.post('/add', auth(), cartCtrl.addToCart);
+router.get('/get', auth(), cartCtrl.getCart);
+router.post('/sync', auth(), cartCtrl.syncCartFromLocalStorage);
+router.delete('/remove/:productId', auth(), cartCtrl.removeFromCart);
 
 
 module.exports = router;
